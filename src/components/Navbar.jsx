40cd5a25b50c37f3ff975afc55485da434997ec9
Lucia Tello <tellolucia87@gmail.com>
2022-11-07 21:07:00 -0300

    Pre entrega2 Tello, link al brand y corrigiendo category insecticidas

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,9 +16,9 @@ function Nabvar() {
   return (
     <Navbar style={{backgroundImage: "linear-gradient(to right, rgb(9, 99, 39), rgb(52, 187, 59))"}} expand="lg">
       <Container>
-      <a className="click-logo" href="/">
+      <Link className="click-logo" to="/">
         <img style={{width:'12rem' }} className="nabvar__logo" src={logo} alt="logonavbar"/>
-      </a>
+      </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -26,7 +26,7 @@ function Nabvar() {
             <NavDropdown className='link-productos'  title="Productos" id="basic-nav-dropdown">
               <Link className='link-categoria' to="/category/fertilizantes">Fertilizantes</Link>
               <Link className='link-categoria' to="/category/herbicidas">Herbicidas</Link>
-              <Link className='link-categoria' to="/category/Insecticidas">Insecticidas</Link>
+              <Link className='link-categoria' to="/category/insecticidas">Insecticidas</Link>
             </NavDropdown>
             <Link style={{ color:'#000',fontWeight:'bold'}} className='link-contacto' to="/contacto">Contacto</Link>
           </Nav>
