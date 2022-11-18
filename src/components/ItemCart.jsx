4e2294798fbfb468890fc1a64c5876c8feb2076b
Lucia Tello <tellolucia87@gmail.com>
2022-11-18 08:48:00 -0300

    agregando itemcart

--- src/components/ItemCart.jsx
+++ src/components/ItemCart.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './ItemCart.css';
+import { useContext } from 'react'
+import { cartContext } from './CartContext'
 
-export default function ItemCart() {
+export default function ItemCart({product}) {
+
+    const {removeItem} = useContext(cartContext);
   return (
-    <div>ItemCart</div>
-  )
+    <div>
+        <Card className='cardCart' style={{ width: '20rem' }}>
+      <Card.Img variant="top" src={product.image} alt={product.image} />
+      <Card.Body>
+        <Card.Title>{product.name}</Card.Title>
+        <Card.Subtitle>{product.description}</Card.Subtitle>
+        <Card.Text>Cantidad {product.cantidad}</Card.Text>
+        <Card.Text>Precio USD {product.price}</Card.Text>
+        <Card.Text>Subtotal USD {product.cantidad * product.price}</Card.Text>
+        <Button variant="success" onClick={()=> removeItem(product.id)}>Eliminar producto</Button>
+      </Card.Body>
+    </Card>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDb5MTBClARPjtD6lapp4onkhDvJDUYqSw",
+  authDomain: "greenspacessolutions-tello.firebaseapp.com",
+  projectId: "greenspacessolutions-tello",
+  storageBucket: "greenspacessolutions-tello.appspot.com",
+  messagingSenderId: "963212555822",
+  appId: "1:963212555822:web:412784ba9faaad4f7411bf"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
